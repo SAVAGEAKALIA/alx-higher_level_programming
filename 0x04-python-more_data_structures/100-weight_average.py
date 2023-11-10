3b853d9cec358fe0ccbb5238542e1af31136331d
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-11-10 02:44:36 +0100

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list or not isinstance(my_list, list):
+    if not my_list:
         return 0
 
-    total_weighted_sum = 0
+    total_score = 0
     total_weight = 0
 
     for score, weight in my_list:
-        if not isinstance(score, (int, float)) or not isinstance(weight, (int, float)) or weight < 0:
-            return 0
-
-        total_weighted_sum += score * weight
+        total_score += score * weight
         total_weight += weight
 
     if total_weight == 0:
         return 0
 
-    weighted_average = total_weighted_sum / total_weight
-    return weighted_average
+    average = total_score / total_weight
+    return average
