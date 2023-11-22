723a5d825a9a0e673a3ca84ee0ae134e314ef391
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-11-22 05:55:04 +0100

    done 2

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,12 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        result = "None"
-    else:
-        return result
+        # Handle division by zero
+        result = None
+    except (TypeError, ValueError):
+        # Handle wrong type
+        result = None
     finally:
+        # Print the result inside the finally block
         print("Inside result: {}".format(result))
+        return result
