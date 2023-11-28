19fa2746c785996b67254ef0228a89c7bab90f80
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-11-28 07:06:15 +0100

    =ok

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Function that adds two integers """
+
+
+def add_integer(a, b=98):
+    """add_integer adds two integer in a function """
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" A matrix Function with Test """
+
+
+def matrix_divided(matrix, div):
+    """ matrix function
+
+    round figure to be divided by idv
+
+    """
+
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        er = "matrix must be a matrix (list of lists) of integers/floats"
+        raise TypeError("{}".format(er))
+
+    if not all(isinstance(row, list) or len(row) == 0 for row in matrix):
+        err = "matrix must be a matrix (list of lists) of integers/floats"
+        raise TypeError("{}".format(err))
+
+    if not \
+       all(isinstance(elem, (int, float)) for row in matrix for elem in row):
+        err = "matrix must be a matrix (list of lists) of integers/floats"
+        raise TypeError("{}".format(err))
+
+    row_length = len(matrix[0])
+    if not all(len(row) == row_length for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if any(not row for row in matrix):
+        err = "matrix must be a matrix (list of lists) of integers/floats"
+        raise TypeError("{}".format(err))
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    result = [[round((i / div), 2) for i in row] for row in matrix]
+    return result
