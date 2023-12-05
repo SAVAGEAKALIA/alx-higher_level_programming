370fafb2351c2102926d44712f1f1f6ea749de0c
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-12-05 19:19:33 +0100

    Finished4

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """ A locked Class documentation """
+
+
 class LockedClass:
     """
-    A class with restricted attribute creation, allowing only 'first_name'.
+    A class with restricted attr creation, allowing only 'first_name'.
 
     Attributes:
     - __slots__: A list specifying the allowed attribute names.
 
     Methods:
-    - __init__(self, first_name): Constructor to initialize the 'first_name' attribute.
+    (self, first_name): Constructor to init the 'first_name' attr.
     """
 
     __slots__ = ['first_name']
 
-    def __init__(self, first_name=''):
+    def __init__(self, first_name = ''):
         """
         Initializes an instance of LockedClass.
 
