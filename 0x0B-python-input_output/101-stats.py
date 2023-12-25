68f75bb057ad08e9ca2234e82d10901e5ed27182
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-12-25 14:28:26 +0100

    Input/output advance6

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,7 +4,7 @@ import sys
 
 # Initialize variables to store metrics
 total_size = 0
-status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+stats_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
 
 try:
@@ -21,15 +21,15 @@ try:
             total_size += file_size
 
             # Update the count for the status code
-            if status_code in status_counts:
-                status_counts[status_code] += 1
+            if status_code in stats_count:
+                stats_count[status_code] += 1
 
             line_count += 1
 
         # Print metrics every 10 lines
         if line_count % 10 == 0:
             print("""File size: {}""".format(total_size))
-            for code, count in sorted(status_counts.items()):
+            for code, count in sorted(stats_count.items()):
                 if count > 0:
                     print("""{}: {}""".format(code, count))
 
@@ -38,6 +38,6 @@ except KeyboardInterrupt:
 finally:
     """ Print final metrics after interruption """
     print("""File size: {}""".format(total_size))
-    for code, count in sorted(status_counts.items()):
+    for code, count in sorted(stats_count.items()):
         if count > 0:
             print("""{}: {}""".format(code, count))
