1ff43f596d10c4327548cf9af46f432c75462af2
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-01-24 16:26:11 +0100

    more queries 1

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,9 +1,9 @@
 -- import dump from hbtn_0d_tvshows to your MySQL server
 -- Write a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
-SELECT tv_genres.name, COALESCE(SUM(tv_show_ratings.rate), 0) AS rating_sum
+SELECT tv_genres.name, COALESCE(SUM(tv_show_ratings.rate), 0) AS rating
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_genres.id, tv_genres.name
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
