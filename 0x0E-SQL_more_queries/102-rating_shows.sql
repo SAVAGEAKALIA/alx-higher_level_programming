be2dc60ce1f443a1fa7828d9c26f26671d6dc74a
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-01-24 16:27:41 +0100

    more queries 2

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- import dump from hbtn_0d_tvshows to your MySQL server
 -- Write a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT tv_shows.title, COALESCE(SUM(tv_show_ratings.rate), 0) AS rating_sum
+SELECT tv_shows.title, COALESCE(SUM(tv_show_ratings.rate), 0) AS rating
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.id, tv_shows.title
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
