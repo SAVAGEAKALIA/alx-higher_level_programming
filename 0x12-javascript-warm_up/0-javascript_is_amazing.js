7c5a0e8470ee36c780619c4c7223859ab270443e
Saviour <akaliasaviour@gmail.com>
2024-04-09 09:19:35 +0100

    Java Script

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,4 @@
+#!/bin/usr/env node
+
+let java = "JavaScript is amazing";
+console.log(java)
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/bin/usr/env node
+
+let multi = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < multi.length; i++) {
+    console.log(multi[i]);
+}
