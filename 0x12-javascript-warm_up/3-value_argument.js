d5b1db3279c585c5b9b7932e86ed0eafb6546bba
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-04-10 18:24:53 +0100

    Java Script

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,16 +3,27 @@
 const process = require('process');
 
 const args = process.argv.slice(2).join(' ');
+let isadd = false;
 
 if (!args) {
   console.log('No argument');
 } else if (args.includes(' ')) {
   const argssplit = args.split(' ');
   let i = 0;
-  while (argssplit[i] !== undefined) {
-    console.log(argssplit[i]);
+  let output2 = '';
+  while (argssplit[i]) {
+    let output = '';
+    output += argssplit[i];
+
+    if (isadd) {
+      output2 += ' ' + output;
+    } else {
+      output2 += output;
+    }
     i++;
+    isadd = true;
   }
+  console.log(output2);
 } else {
   console.log(args);
 }
