9ef155522ab25abfaa4ba3dcb90729c5591643ec
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-06-20 07:53:16 +0100

    Javafix4

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,11 +3,11 @@
 const process = require('process');
 
 const args = process.argv.slice(2).join(' ');
-let arg
+let ar
 
 if (!args) {
   console.log('No argument');
 } else {
-  arg = args.split(' ');
-  console.log(arg[0]);
+  ar = args.split(' ');
+  console.log(ar[0]);
 }
