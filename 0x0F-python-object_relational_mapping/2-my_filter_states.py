eaf7d8c2cd66a84c89883b70047bcc7bdd3eeadd
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-06-25 08:19:45 +0100

    Python ORM

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,51 @@
 #!/usr/bin/python3
 """
-SQL ORM FOR PYTHON DATABASES
-MySqldb
+SQL ORM for Python Databases using MySQLdb
+
+This script connects to a MySQL database and retrieves rows from the 'states'
+table where the 'name' matches a user-provided argument. The results are
+ordered by 'id'.
+
+Usage:
+    ./script_name.py <username> <password> <database> <state_name>
+
+Example:
+    ./script_name.py root mypassword mydatabase Texas
 """
 
 import MySQLdb
 import sys
 
-args = sys.argv[1:]
 
-username, password, database, arg = args
+def main():
+    # Get command-line arguments
+    args = sys.argv[1:]
 
-db = MySQLdb.connect(host="localhost", port=3306,
-                     user=username, passwd=password, db=database)
+    if len(args) != 4:
+        print("Usage: ./script_name.py <username> <password> <database> <state_name>")
+        sys.exit(1)
 
-cur = db.cursor()
+    username, password, database, state_name = args
 
-cur.execute("SELECT * FROM states "
-            "WHERE name = %s ORDER BY states.id;", (arg,))
+    # Connect to the MySQL database
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
 
-for row in cur.fetchall():
-    print(row)
+    # Create a cursor object to interact with the database
+    cur = db.cursor()
 
-cur.close()
-db.close()
+    # Execute a parameterized query to prevent SQL injection
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id;"
+    cur.execute(query, (state_name,))
+
+    # Fetch and print all rows from the executed query
+    for row in cur.fetchall():
+        print(row)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
+
 
 if __name__ == "__main__":
-    pass
+    main()
