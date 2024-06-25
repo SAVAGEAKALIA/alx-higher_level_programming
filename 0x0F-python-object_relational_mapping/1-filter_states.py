2ce5ba95a1975bde3a214d6a0ed6941a8c7f22af
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-06-25 09:24:06 +0100

    Python ORM3

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -28,7 +28,7 @@ def main():
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states "
-                "WHERE LOWER(name) LIKE %s ORDER BY states.id;", ("%n%",))
+                "WHERE  LOWER(name) LIKE LOWER('N%') ORDER BY states.id;")
 
     # Fetch all rows from the executed query and print each row
     for row in cur.fetchall():
