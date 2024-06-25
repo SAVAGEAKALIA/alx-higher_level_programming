07c4bdfc01778371ed1e4b9e96b7407fd5e1f107
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-06-25 11:44:56 +0100

    Python ORM3

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+""" Python Database Using Sqlalchemy """
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
+    """ Data base code testing Sql """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(50))
