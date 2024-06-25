98128d77e9dcd57c315a09e06dae289694a8a633
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-06-25 08:39:19 +0100

    Python ORM3

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-""" SQL ORM FOR PYTHON DATABASES """
+"""
+Script that takes in an argument and
+displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+
 import MySQLdb
 import sys
 
