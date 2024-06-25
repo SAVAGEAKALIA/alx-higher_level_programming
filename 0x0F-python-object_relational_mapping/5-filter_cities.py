6a14893e52083c567a4a550b4a610858798fbf81
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-06-25 10:21:49 +0100

    Python ORM3

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -39,8 +39,8 @@ def main():
     # Create a cursor object to interact with the database
     cur = db.cursor()
 
-    cur.execute("SELECT cities.name FROM cities, states "
-                "JOIN states ON cities.state_id = states.id "
+    cur.execute("SELECT cities.name FROM cities "
+                "INNER JOIN states ON cities.state_id = states.id "
                 "WHERE states.name = %s ORDER BY cities.id ASC;", (args,))
 
     # Fetch all rows from the executed query
