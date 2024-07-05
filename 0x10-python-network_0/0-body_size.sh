051b3b3b7e2fb0f8fdd8d837ccf149249cda5346
Saviour <akaliasaviour@gmail.com>
2024-07-05 10:17:14 +0100

    network basics

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+if [ $# -ne 1 ]; then 
+	echo "Arguments for server not complete"
+	exit 1
+fi
+
+IP=$1
+
+# Call Curl command and pass to grep to print number of lines
+
+curl -sI "$IP" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r'
