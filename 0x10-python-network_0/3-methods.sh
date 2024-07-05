3a52eebf9b7ec8a71120ce34655506715cc57b66
Saviour <akaliasaviour@gmail.com>
2024-07-05 14:10:38 +0100

    networking_task

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -sI -X OPTIONS "$URL" | grep "Allow:" | cut -d ' ' -f2-
+curl -sI -X OPTIONS "$URL" -
