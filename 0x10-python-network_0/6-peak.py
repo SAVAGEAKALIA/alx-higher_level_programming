cba6e467f81776fb9e237e41dc867ec183e43446
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-07-06 09:18:30 +0100

    Network 0 done

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Script to calculate peak """
+
+
+def find_peak(list_of_integers):
+    """Function  definition"""
+
+    num_list = list_of_integers
+
+    if len(num_list) == 0:
+        return None
+
+    if len(num_list) == 1:
+        return num_list[0]
+    if len(num_list) == 2:
+        return max(num_list)
+
+    middle_index = len(num_list) // 2
+
+    right = middle_index + 1
+    left = middle_index - 1
+
+    if num_list[middle_index] > num_list[middle_index - 1] and num_list[middle_index] > num_list[middle_index + 1]:
+        return num_list[middle_index]
+
+    elif num_list[right] > num_list[middle_index]:
+        for element in num_list[right:]:
+            if element > num_list[right]:
+                num_list[right] = element
+            else:
+                continue
+        return num_list[right]
+    else:
+        for element in num_list[:left]:
+            if element > num_list[left]:
+                num_list[left] = element
+            else:
+                continue
+        return num_list[left]
