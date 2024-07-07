055e90497a15eb8c35953770b5fec6a81c243544
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-07-07 18:58:57 +0100

    Network 1 task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,14 +8,12 @@ header from a URL provided as a command-line argument.
 from urllib.request import urlopen, Request
 from sys import argv
 
-arg1 = argv[1]
-
 
 def main():
     """
     Main function to execute the script.
     """
-
+    arg1 = argv[1]
     req = Request(arg1)
 
     with urlopen(req) as response:
