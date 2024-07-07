41f5cc3f670a2a6c591ef8bf824771eab2574622
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-07-07 17:27:12 +0100

    Mistake

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+const args = process.argv.slice(2).join(' ');
+let ar;
+
+if (!args) {
+  console.log('No argument');
+} else {
+  ar = args.split(' ');
+  console.log(ar[0]);
+}
