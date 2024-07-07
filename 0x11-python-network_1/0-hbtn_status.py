b988325786e29d694318a18f511ffa838ff8fd19
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-07-07 18:54:39 +0100

    Network 1 task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,13 +7,12 @@ from urllib.request import urlopen
 def main():
     """
     Write a Python script that
-    fetches https://alx-intranet.hbtn.io/status
+    fetches alx-intranet.hbtn.io/status
     """
     url = "https://alx-intranet.hbtn.io/status"
 
     with urlopen(url) as e:
         req = e.read()
-
 
     print("Body response:")
     print(f"\t- type: {type(req)}")
