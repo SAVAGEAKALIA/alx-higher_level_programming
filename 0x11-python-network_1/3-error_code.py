ca5046018b95b1441dd20b88521ca4f8cbc03685
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-07-08 05:27:52 +0100

    Network 1 task

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,30 +2,28 @@
 """ Python Network 01 """
 
 from urllib.request import Request, urlopen
-from urllib.parse import urlencode
+from urllib.error import HTTPError
 from sys import argv
 
 
 def main():
     """
     Write a Python script that
-    Sends a Post Request
+    Checks for error codes
     Use print statement to output answer
     """
     url = argv[1]
-    email = argv[2]
 
-    dict_email = {'email': email}
-    req = urlencode(dict_email)
-    req = req.encode('ascii')
+    try:
+        # data_req = Request(url, method='POST')
 
-    data_req = Request(url, data=req, method='POST')
+        with urlopen(url) as response:
+            html = response.read()
 
-    with urlopen(data_req) as response:
-        html = response.read()
-
-    html = html.decode('utf-8')
-    print(f'Your email is: {html}')
+        html = html.decode('utf-8')
+        print(f'{html}')
+    except HTTPError as e:
+        print(f'Error code: {e.code}')
 
 
 if __name__ == '__main__':
