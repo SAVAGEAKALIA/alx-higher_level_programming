a74740625b67f08a6489b8c4c8c0ae3382361752
Saviour <akaliasaviour@gmail.com>
2024-08-03 06:07:57 +0100

    web scraping 5&6

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,29 +2,27 @@
 const process = require('process');
 const fs = require('fs');
 const request = require('request');
-let site = process.argv.slice(2)[0]
-let filepath = process.argv.slice(2)[1];
+const site = process.argv.slice(2)[0];
+const filepath = process.argv.slice(2)[1];
 
 if (!site || !filepath) {
-    console.error('Usage: node fetch_and_save.js <site_url> <filepath>');
-    process.exit(1);
+  console.error('Usage: node fetch_and_save.js <site_url> <filepath>');
+  process.exit(1);
 }
 
 request(site, (error, response, body) => {
-    if (error) {
-        console.error(`Error: ${error}`);
-        process.exit(1);
+  if (error) {
+    console.error(`Error: ${error}`);
+    process.exit(1);
+  }
+  if (response.statusCode !== 200) {
+    console.error(`Error: Received HTTP status code ${response.statusCode}`);
+    process.exit(1);
+  }
+  fs.writeFile(filepath, body, (err) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
     }
-    if (response.statusCode !== 200) {
-        console.error(`Error: Received HTTP status code ${response.statusCode}`);
-        process.exit(1);
-    }
-    fs.writeFile(filepath, body, (err) => {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-
-        console.log(${filepath});
-    });
-})
+  });
+});
