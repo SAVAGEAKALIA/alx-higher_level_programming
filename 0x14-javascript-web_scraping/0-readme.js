1aa3e1b13dd128430345e9b611237c09a229f85f
Saviour <akaliasaviour@gmail.com>
2024-08-03 02:33:25 +0100

    web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-const fs = require('fs')
-const process = require('process')
-ar = process.argv.slice(2)[0]
+const fs = require('fs');
+const process = require('process');
+const ar = process.argv.slice(2)[0];
 
 if (!ar) {
-    console.error('Please provide a file path as an argument');
-    process.exit(1);
+  console.error('Please provide a file path as an argument');
+  process.exit(1);
 }
-fs.readFile(ar, "utf-8", (err, data) => {
-    if (!err) {
-        console.log(data);
-    } else {
-        console.log(err);
-    }
-})
+fs.readFile(ar, 'utf-8', (err, data) => {
+  if (!err) {
+    console.log(data);
+  } else {
+    console.log(err);
+  }
+});
