1df687beb5ec0c48dd13ec376ff824d5a45faf84
Saviour <akaliasaviour@gmail.com>
2024-08-03 03:56:13 +0100

    web scraping 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
-const request = require('request')
+const request = require('request');
 const process = require('process');
-movie_id = process.argv.slice(2)[0];
-if (movie_id === undefined) {
-    console.error('Usage: node script.js <movie_id>');
-    process.exit(1);
+const movieId = process.argv.slice(2)[0];
+if (movieId === undefined) {
+  console.error('Usage: node script.js <movie_id>');
+  process.exit(1);
 }
 
-const api = `https://swapi-api.alx-tools.com/api/films/${movie_id}`
+const api = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 console.log(api);
 request.get(api, (err, response, body) => {
-    if (err) {
-        console.log(err);
-        process.exit(1)
-    }
-    if (!response && response.statusCode === 200) {
-        console.error(`Error: ${response.statusCode}`);
-        process.exit(1)
-    }
-    const movie = JSON.parse(body)
-    console.log(`Title: ${movie.title}`);
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  if (!response && response.statusCode === 200) {
+    console.error(`Error: ${response.statusCode}`);
+    process.exit(1);
+  }
+  const movie = JSON.parse(body);
+  console.log(`Title: ${movie.title}`);
 });
