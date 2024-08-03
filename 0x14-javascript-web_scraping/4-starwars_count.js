09019bfb65df20b92ca3579776f8e802fa7bd174
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-08-03 04:57:14 +0100

    Web_scraping 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+let movieapi = process.argv[2]
+console.log(movieapi)
+
+if (!movieapi) {
+    console.error('Usage: node 0-fetch-character.js <character_id>');
+    process.exit(1);
+}
+characterId = 18;
+
+
+request.get(movieapi, (err, response, body) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    if (response.statusCode !== 200) {
+        console.error(`Request failed with status code ${response.statusCode}`);
+        process.exit(1);
+    }
+
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const film of films) {
+        const characters = film.characters
+        for (const character of characters) {
+            if (character.includes(characterId)) {
+                count++;
+                break;
+            }
+        }
+    }
+    console.log(count);
+});
