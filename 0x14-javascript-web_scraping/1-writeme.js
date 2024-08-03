c310904cfb4e2a44f16334087143f4a97c03089e
Saviour <akaliasaviour@gmail.com>
2024-08-03 02:44:36 +0100

    web scraping 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-const fs = require('fs')
-const process = require('process')
-let args = process.argv.slice(2)[0]
-let input = process.argv.slice(2)[1]
+const fs = require('fs');
+const process = require('process');
+const args = process.argv.slice(2)[0];
+const input = process.argv.slice(2)[1];
 
 if (args === undefined) {
-    console.error('Usage: file not given')
-    process.exit(1)
+  console.error('Usage: file not given');
+  process.exit(1);
 }
 fs.writeFile(args, input, (err) => {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-})
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+});
