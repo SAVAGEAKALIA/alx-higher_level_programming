c7fa0a7f0065a9fa7ad40b0a858df01dd634bb6b
Saviour <akaliasaviour@gmail.com>
2024-08-03 03:03:38 +0100

    web scraping 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 const process = require('process');
-let args = process.argv.slice(2)[0]
+const args = process.argv.slice(2)[0];
 
 if (args === undefined) {
-    console.log('Usage: node script.js <API URL>');
-    process.exit(1);
+  console.log('Usage: node script.js <API URL>');
+  process.exit(1);
 }
 
 request.get(args).on('response', (response) => {
-    console.log('code: ' + response.statusCode);
-})
+  console.log('code: ' + response.statusCode);
+});
